bd5bc9576ba54c7c73e1e9e6a3df1c33775d89c9
watchdog1023 <Naickercd@gmail.com>
2020-05-17 14:15:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from telegram.ext import Updater, CommandHandler
 import requests
 import re
+import os
 
-#1228609641:AAEiAwIeyqAByW95S_OxWFITi0JUu9HjyGU
+#1228609641:AAEiAwIeyqAByW95S_OxWFITi0JUu9HjyGU tester
+#1156004345:AAGYIogVoAMSter7H_RUrQIiJPb_dLVfl_w allfather
+
+def echo(update, context):
+    chat_id = update.message.chat_id
+    command = context.args[0].lower()
+    print(command)
+    os.system("python3 phdler.py custom " + command)
+
 
 def bop(bot, update):
     chat_id = update.message.chat_id
     #bot.send_message(chat_id=chat_id,text="AllFATHER!")
-    text_caps = ' '.join(context.args).upper()
-    bot.send_message(chat_id=chat_id,text="text_caps")
-    bot.send_video(chat_id=chat_id,video=open('test.mp4', 'rb'), supports_streaming=True)
+    bot.send_video(chat_id=chat_id,video=open('handpicked/*.mp4', 'rb'), supports_streaming=True)    
 
 def main():
-    updater = Updater('1156004345:AAGYIogVoAMSter7H_RUrQIiJPb_dLVfl_w')
+    updater = Updater('1228609641:AAEiAwIeyqAByW95S_OxWFITi0JUu9HjyGU',use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('hi',bop))
+    dp.add_handler(CommandHandler('hi',echo))
+    dp.add_handler(CommandHandler('send',bop))
     updater.start_polling()
     updater.idle()
 
